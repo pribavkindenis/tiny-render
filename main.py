c509972e1e781799d14f363b5523d152cd477b70
Denis Pribavkin <pribavkindenis@gmail.com>
2019-04-16 07:46:43 +0400

    Diablo model added

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from model.model import Model
 import numpy as np
 
 
-if __name__ == '__main__':
-    model = Model()
+def render(model: Model, save_path: str):
     image = Image(800, 800)
     render_model(model,
                  image,
                  np.array([0, 0, 1]),
                  np.array([2, 0.5, 5]),
                  np.array([0, 0, 0]))
-    image.save_image()
+    image.save_image(save_path)
+
+
+if __name__ == '__main__':
+    render(Model(), "african_head.png")
+    render(Model("./obj/diablo/diablo.obj", "./obj/diablo/diablo_diffuse.tga"), "diablo.png")
